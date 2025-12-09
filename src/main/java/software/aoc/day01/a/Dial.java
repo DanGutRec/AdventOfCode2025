392f713d3a56a4e74f87bdf6d1bd4a62aa0d90d6
Dani <164520978+DanGutRec@users.noreply.github.com>
2025-12-09 16:50:31 +0000

    Dial class implements single movement and its tests

--- src/main/java/software/aoc/day01/a/Dial.java
+++ src/main/java/software/aoc/day01/a/Dial.java
@@ -1,4 +1,47 @@
 package software.aoc.day01.a;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Dial {
+    private final ArrayList<Integer> orders;
+    private Dial() {
+        this.orders = new ArrayList<Integer>();
+    }
+    public  static Dial create() {
+        return new Dial();
+    }
+    public int position() {
+        return sumAll();
+    }
+
+    private int sumAll() {
+        return normalize(orders.stream().mapToInt(x -> x).sum()+50);
+    }
+
+    private int normalize(int value) {
+        return (value < 0 ? 100 : 0)+value % 100;
+    }
+
+    public Dial execution(String orders) {
+        return add(orders.split("\n"));
+    }
+
+    public Dial add(String... split) {
+        Arrays.stream(split).map(this::conversion).forEach(orders::add);
+        return this;
+    }
+
+    private int conversion(String line) {
+        return signOf(line)*parseInt(line);
+    }
+
+    private int parseInt(String line) {
+        return Integer.parseInt(line.substring(1));
+    }
+
+    private int signOf(String line) {
+        return line.charAt(0) == 'R' ? 1 : -1;
+    }
 }
--- src/test/java/test/Day01ATest.java
+++ src/test/java/test/Day01ATest.java
@@ -1,4 +1,25 @@
 package test;
 
+import org.junit.Test;
+import software.aoc.day01.a.Dial;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Day01ATest {
+    @Test
+    public void Dial_test_positions_simple_movements() {
+        assertThat(Dial.create().position()).isEqualTo(50);
+        assertThat(Dial.create().execution("R10").position()).isEqualTo(60);
+        assertThat(Dial.create().execution("R49").position()).isEqualTo(99);
+        assertThat(Dial.create().execution("R50").position()).isEqualTo(0);
+        assertThat(Dial.create().execution("L10").position()).isEqualTo(40);
+        assertThat(Dial.create().execution("L50").position()).isEqualTo(0);
+        assertThat(Dial.create().execution("L51").position()).isEqualTo(99);
+    }
+    @Test
+    public void Dial_test_positions_multiple_movements() {
+        assertThat(Dial.create().execution("R10\nR10").position()).isEqualTo(70);
+        assertThat(Dial.create().add("R10","R10").position()).isEqualTo(70);
+
+    }
 }
