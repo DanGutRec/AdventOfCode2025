7096b1984682779d215f9142fa48f0a4fc381a61
Dani <164520978+DanGutRec@users.noreply.github.com>
2025-12-10 19:46:49 +0000

    Day1A implementation of the method countPositionIsOn0 and its tests

--- src/main/java/software/aoc/day01/a/model/Dial.java
+++ src/main/java/software/aoc/day01/a/model/Dial.java
@@ -2,6 +2,7 @@ package software.aoc.day01.a.model;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Dial {
     private final ArrayList<Order> orders;
@@ -11,27 +12,31 @@ public class Dial {
     public  static Dial create() {
         return new Dial();
     }
-    public int position() {
-        return sumAll();
-    }
-
-    private int sumAll() {
-        return normalize(orders.stream().mapToInt(Order::movement).sum()+50);
-    }
-    private int normalize(int value) {
-        return (value < 0 ? 100 : 0)+value % 100;
-    }
-
     public Dial execution(String orders) {
         return add(orders.split("\n"));
     }
-
     public Dial add(String... split) {
         Arrays.stream(split).map(this::orderFactory).forEach(orders::add);
         return this;
     }
-
     private Order orderFactory(String line) {
         return Order.create(line);
     }
+    private int normalize(int value) {
+        return (value < 0 ? 100 : 0)+value % 100;
+    }
+    private int sumAll() {
+        return normalize(orders.stream().mapToInt(Order::movement).sum()+50);
+    }
+    public int position() {
+        return sumAll();
+    }
+    public int countPositionIsOn0(){
+        return (int) IntStream.range(0,orders.size()+1).map(this::sumSubOrders).filter(position-> position ==0).count();
+    }
+
+    private int sumSubOrders(int position) {
+        return normalize(orders.stream().limit(position).mapToInt(Order::movement).sum()+50);
+    }
+
 }
--- src/test/java/test/Day01ATest.java
+++ src/test/java/test/Day01ATest.java
@@ -2,10 +2,16 @@ package test;
 
 import org.junit.Test;
 import software.aoc.day01.a.model.Dial;
+import software.aoc.day01.a.model.Order;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Day01ATest {
+    @Test
+    public void Order_test_creation() {
+        assertThat(Order.create("R10").movement()).isEqualTo(10);
+        assertThat(Order.create("L10").movement()).isEqualTo(-10);
+    }
     @Test
     public void Dial_test_positions_simple_movements() {
         assertThat(Dial.create().position()).isEqualTo(50);
@@ -24,5 +30,12 @@ public class Day01ATest {
         assertThat(Dial.create().execution("L10\nL10").position()).isEqualTo(30);
         assertThat(Dial.create().add("L10","L10").position()).isEqualTo(30);
         assertThat(Dial.create().execution("L50\nL50").position()).isEqualTo(50);
+    }
+    @Test
+    public void Dial_test_count_how_many_time_is_on_the_position_0() {
+        assertThat(Dial.create().execution("R10\nR10").countPositionIsOn0()).isEqualTo(0);
+        assertThat(Dial.create().execution("R50\nR10").countPositionIsOn0()).isEqualTo(1);
+        assertThat(Dial.create().execution("L50\nL10").countPositionIsOn0()).isEqualTo(1);
+        assertThat(Dial.create().execution("R50\nR100").countPositionIsOn0()).isEqualTo(2);
     }
 }
